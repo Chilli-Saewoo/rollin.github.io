214d4aad24cd3e18001cc5e232175c042b052a61
Haru <kkang47140@gmail.com>
2022-10-27 01:28:38 +0900

    [feat]작성페이지 logic 구현입니다

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useEffect, useState } from 'react-router-dom';
 import SendPage from './pages/sendPage';
 import { WritePage } from './pages/writePage';
 import { ref, set, get, update, remove, child} from "firebase/database";
 
 const App = () => {
+  
   return (
     <Routes>
-      <Route path="/write" element={<WritePage />} />
-      <Route path="/send" element={<SendPage />} />
+      <Route path='/write/id=28C9D618-4D7A-48EE-B86E-79B597BF0E1B' element={<WritePage />} />
+      <Route path='/send'  element={<SendPage />} />
     </Routes>
   );
 };
--- src/pages/writePage.js
+++ src/pages/writePage.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import StartFirebase from "../firebase";
-import { ref, set, get, update, remove, child } from "firebase/database";
+import { ref, set, push, get, update, remove, child, onValue, getDatabase } from "firebase/database";
 import { Link } from 'react-router-dom';
 import TextInput from "../components/TextInput";
 import styledComponents from "styled-components";
@@ -27,19 +27,22 @@ import skullBackPurple from '../img/skullBackPurple.png';
 
 import uuid from "react-uuid";
 import qs from "query-string";
+import styles from "../App.css"
 
 
 export class WritePage extends React.Component {
-    
+
     constructor(props) {
         super(props);
         this.state = {
             db: ' ',
             // UUID: ' ',
-            // image: ' ',
+            image: ' ',
             // nickname: ' ',
             message: ' ',
-            sender: ' '
+            sender: ' ',
+            data: ' '
+            
         }
         this.interface = this.interface.bind(this)
     }
@@ -51,72 +54,86 @@ export class WritePage extends React.Component {
     }
 
     render() {
-      const parent  = { width: '100%', height: '100px', padding: '12px 0 0 0'}
-      const child = { width: '100%', height: '100px', padding: '0 4px 0 0'}
+        const parent = { width: '100%', height: '100px', padding: '12px 0 0 0' }
+        const child = { width: '100%', height: '100px', padding: '0 4px 0 0' }
 
-      const clickSticker = event => {
-        const active = document.querySelector(".selected");
-        active.classList.remove("selected");
-        active.classList.add("notSelected")
-        const target = event.target.nodeName === "IMAGE" ? event.target : event.target.parentNode;
-        target.classList.add("selected");
-        target.classList.remove("notSelected");
-        const sticker = document.querySelector("#"+target.id+"Sticker");
-        const shownSticker = document.querySelector(".isShown")
-        shownSticker.classList.remove("isShown")
-        shownSticker.classList.add("isHidden")
-        sticker.classList.add("isShown")
-        sticker.classList.remove("isHidden")
+        const clickSticker = event => {
+            const active = document.querySelector(".selected");
+            active.classList.remove("selected");
+            active.classList.add("notSelected")
+            const target = event.target.nodeName === "IMAGE" ? event.target : event.target.parentNode;
+            target.classList.add("selected");
+            target.classList.remove("notSelected");
+            const sticker = document.querySelector("#" + target.id + "Sticker");
+            const shownSticker = document.querySelector(".isShown")
+            shownSticker.classList.remove("isShown")
+            shownSticker.classList.add("isHidden")
+            sticker.classList.add("isShown")
+            sticker.classList.remove("isHidden")
+
+        };
+
+        const db = StartFirebase();
+        var data;
+        var link = document.location.href.split("=");
         
-      };
+        const CountRef = ref(db, 'users/' + link[1] + '/nickname');
+        onValue(CountRef, (snapshot) => {
+            data = snapshot.val();
+            console.log(data);
+        });
 
         return (
-            <div style={{maxWidth: '100%', overflowX: 'hidden'}}>
+            <div style={{ maxWidth: '100%', overflowX: 'hidden' }}>
                 <p style={{
                     fontSize: '24px'
-                }}>To. 니쿠니쿠닉</p>
+                }}>To. {data}</p>
                 <p style={{
                     marginTop: '24px'
                 }}>작성할 롤링페이퍼의 스티커를 선택해주세요</p>
                 <div className="tags" style={parent}>
-                    <div id="pumpkinOrange" className="tag selected" style={child}><img src={pumpkinOrange} style={{ marginTop: '12px', width: '50px', height: '55px', marginLeft: '14px' }} onClick={clickSticker} /></div>
-                    <div id="batOrange" className="tag notSelected" style={child}><img src={batOrange} style={{ marginTop: '12px', width: '50px', height: '55px', marginLeft: '14px' }} onClick={clickSticker} /></div>
-                    <div id="carriageOrange" className="tag notSelected" style={child}><img src={carriageOrange} style={{ marginTop: '12px', width: '50px', height: '54px', marginLeft: '14px' }} onClick={clickSticker} /></div>
-                    <div id="ghostOrange" className="tag notSelected" style={child}><img src={ghostOrange} style={{ marginTop: '12px', width: '50px', height: '55px', marginLeft: '14px' }} onClick={clickSticker} /></div>
-                    <div id="skullOrange" className="tag notSelected" style={child}><img src={skullOrange} style={{ marginTop: '12px', width: '50px', height: '55px', marginLeft: '14px' }} onClick={clickSticker} /></div>
-                    <div id="pumpkinPurple" className="tag notSelected" style={child}><img src={batPurple} style={{ marginTop: '12px', width: '50px', height: '55px', marginLeft: '14px' }} onClick={clickSticker} /></div>
-                    <div id="batPurple" className="tag notSelected" style={child}><img src={carriagePurple} style={{ marginTop: '12px', width: '50px', height: '55px', marginLeft: '14px' }} onClick={clickSticker} /></div>
-                    <div id="carriagePurple" className="tag notSelected" style={child}><img src={ghostPurple} style={{ marginTop: '12px', width: '50px', height: '55px', marginLeft: '14px' }} onClick={clickSticker} /></div>
-                    <div id="ghostPurple" className="tag notSelected" style={child}><img src={pumpkinPurple} style={{ marginTop: '12px', width: '50px', height: '55px', marginLeft: '14px' }} onClick={clickSticker} /></div>
-                    <div id="skullPurple" className="tag notSelected" style={child}><img src={skullPurple} style={{ marginTop: '12px', width: '50px', height: '55px', marginLeft: '14px' }} onClick={clickSticker} /></div>
+                    <div id="0" className="tag selected" style={child}><img src={pumpkinOrange} style={{ marginTop: '12px', width: '50px', height: '55px', marginLeft: '14px' }} onClick={clickSticker} /></div>
+                    <div id="1" className="tag notSelected" style={child}><img src={batOrange} style={{ marginTop: '12px', width: '50px', height: '55px', marginLeft: '14px' }} onClick={clickSticker} /></div>
+                    <div id="2" className="tag notSelected" style={child}><img src={carriageOrange} style={{ marginTop: '12px', width: '50px', height: '54px', marginLeft: '14px' }} onClick={clickSticker} /></div>
+                    <div id="3" className="tag notSelected" style={child}><img src={ghostOrange} style={{ marginTop: '12px', width: '50px', height: '55px', marginLeft: '14px' }} onClick={clickSticker} /></div>
+                    <div id="4" className="tag notSelected" style={child}><img src={skullOrange} style={{ marginTop: '12px', width: '50px', height: '55px', marginLeft: '14px' }} onClick={clickSticker} /></div>
+                    <div id="5" className="tag notSelected" style={child}><img src={batPurple} style={{ marginTop: '12px', width: '50px', height: '55px', marginLeft: '14px' }} onClick={clickSticker} /></div>
+                    <div id="6" className="tag notSelected" style={child}><img src={carriagePurple} style={{ marginTop: '12px', width: '50px', height: '55px', marginLeft: '14px' }} onClick={clickSticker} /></div>
+                    <div id="7" className="tag notSelected" style={child}><img src={ghostPurple} style={{ marginTop: '12px', width: '50px', height: '55px', marginLeft: '14px' }} onClick={clickSticker} /></div>
+                    <div id="8" className="tag notSelected" style={child}><img src={pumpkinPurple} style={{ marginTop: '12px', width: '50px', height: '55px', marginLeft: '14px' }} onClick={clickSticker} /></div>
+                    <div id="9" className="tag notSelected" style={child}><img src={skullPurple} style={{ marginTop: '12px', width: '50px', height: '55px', marginLeft: '14px' }} onClick={clickSticker} /></div>
                 </div>
                 <p style={{
                     marginTop: '0px'
                 }}> 스티커에 적을 말을 작성해주세요</p>
-                <img id="pumpkinOrangeSticker" class="sticker isShown" src={pumpkinBackOrange} style={{ marginTop: '12px', width: '100%', height: '100%'}}></img>
-                <img id="batOrangeSticker" class="sticker isHidden" src={batBackOrange} style={{ marginTop: '12px', width: '100%', height: '100%'}}></img>
-                <img id="carriageOrangeSticker" class="sticker isHidden" src={carriageBackOrange} style={{ marginTop: '12px', width: '100%', height: '100%'}}></img>
-                <img id="ghostOrangeSticker" class="sticker isHidden" src={ghostBackOrange} style={{ marginTop: '12px', width: '100%', height: '100%'}}></img>
-                <img id="skullOrangeSticker" class="sticker isHidden" src={skullBackOrange} style={{ marginTop: '12px', width: '100%', height: '100%'}}></img>
-                <img id="pumpkinPurpleSticker" class="sticker isHidden" src={pumpkinBackPurple} style={{ marginTop: '12px', width: '100%', height: '100%'}}></img>
-                <img id="batPurpleSticker" class="sticker isHidden" src={batBackPurple} style={{ marginTop: '12px', width: '100%', height: '100%'}}></img>
-                <img id="carriagePurpleSticker" class="sticker isHidden" src={carriageBackPurple} style={{ marginTop: '12px', width: '100%', height: '100%'}}></img>
-                <img id="ghostPurpleSticker" class="sticker isHidden" src={ghostBackPurple} style={{ marginTop: '12px', width: '100%', height: '100%'}}></img>
-                <img id="skullPurpleSticker" class="sticker isHidden" src={skullBackPurple} style={{ marginTop: '12px', width: '100%', height: '100%'}}></img>
+                <img id="pumpkinOrangeSticker" class="sticker isShown" src={pumpkinBackOrange} style={{ marginTop: '12px', width: '100%', height: '100%' }}></img>
+                <img id="batOrangeSticker" class="sticker isHidden" src={batBackOrange} style={{ marginTop: '12px', width: '100%', height: '100%' }}></img>
+                <img id="carriageOrangeSticker" class="sticker isHidden" src={carriageBackOrange} style={{ marginTop: '12px', width: '100%', height: '100%' }}></img>
+                <img id="ghostOrangeSticker" class="sticker isHidden" src={ghostBackOrange} style={{ marginTop: '12px', width: '100%', height: '100%' }}></img>
+                <img id="skullOrangeSticker" class="sticker isHidden" src={skullBackOrange} style={{ marginTop: '12px', width: '100%', height: '100%' }}></img>
+                <img id="pumpkinPurpleSticker" class="sticker isHidden" src={pumpkinBackPurple} style={{ marginTop: '12px', width: '100%', height: '100%' }}></img>
+                <img id="batPurpleSticker" class="sticker isHidden" src={batBackPurple} style={{ marginTop: '12px', width: '100%', height: '100%' }}></img>
+                <img id="carriagePurpleSticker" class="sticker isHidden" src={carriageBackPurple} style={{ marginTop: '12px', width: '100%', height: '100%' }}></img>
+                <img id="ghostPurpleSticker" class="sticker isHidden" src={ghostBackPurple} style={{ marginTop: '12px', width: '100%', height: '100%' }}></img>
+                <img id="skullPurpleSticker" class="sticker isHidden" src={skullBackPurple} style={{ marginTop: '12px', width: '100%', height: '100%' }}></img>
 
-                <div style={{display: 'table', margin: 'auto', width: '100%'}}>
-                    <textarea type='text' id='sender' value={this.state.message} onChange = {e => {this.setState({message: e.target.value});}}
-                    style={{position: 'relative', top: '-350px', left: '14%', marginTop: '12px', width: '70%', height: '200px', 
-                    background: 'transparent', border: '0', maxlength: '10', fontSize: '1.25em', outlineColor: '#000000', color: '#101010'}}/>
-                    <div style={{position: 'relative', top: '-350px', left: '20%', display: 'flex', backgroundColor: 'transparent', alignItems: 'end'}}>
-                        <p style={{backgroundColor: 'transparent', color: '#202020', fontSize: '1.25em'}}>From.</p>
-                        <input type='text' id='sender' value={this.state.sender} onChange = {e => {this.setState({sender: e.target.value});}}
-                        style={{marginTop: '0px', width: '52%', height: '20px', background: 'transparent', border: '0px', borderBottom: '1px solid #202020', color: '#202020', fontSize: '1.25em'}}/>
+                <div style={{ display: 'table', margin: 'auto', width: '100%' }}>
+                    <textarea type='text' id='sender' value={this.state.message} onChange={e => { this.setState({ message: e.target.value }); }}
+                        style={{
+                            position: 'relative', top: '-350px', left: '14%', marginTop: '12px', width: '70%', height: '200px',
+                            background: 'transparent', border: '0', maxlength: '10', fontSize: '1.25em', outlineColor: '#000000', color: '#101010'
+                        }} />
+                    <div style={{ position: 'relative', top: '-350px', left: '20%', display: 'flex', backgroundColor: 'transparent', alignItems: 'end' }}>
+                        <p style={{ backgroundColor: 'transparent', color: '#202020', fontSize: '1.25em' }}>From.</p>
+                        <input type='text' id='sender' value={this.state.sender} onChange={e => { this.setState({ sender: e.target.value }); }}
+                            style={{ marginTop: '0px', width: '52%', height: '20px', background: 'transparent', border: '0px', borderBottom: '1px solid #202020', color: '#202020', fontSize: '1.25em' }} />
                     </div>
                 </div>
-                <Link to="/send">
-                    <button id = "addBtn" onClick={this.interface} style = {{position: 'relative', top: '-250px', title: '완료', padding:'0px', width:'100%', height:'56px', background:'#FF7242', 
-                    marginTop:'22px', fontSize: '1em', textColor: '#000000', fontWeight: 'bold', border: '0px', borderRadius: '8px'}}>완료</button>
+                <Link to="/send/id=E65EC073-56D0-4DF7-992B-FE9008A74278">
+                    <button id="addBtn" onClick={this.interface} style={{
+                        position: 'relative', top: '-250px', title: '완료', padding: '0px', width: '100%', height: '56px', background: '#FF7242',
+                        marginTop: '22px', fontSize: '1em', textColor: '#000000', fontWeight: 'bold', border: '0px', borderRadius: '8px'
+                    }}>완료</button>
                 </Link>
             </div>
         )
@@ -142,51 +159,23 @@ export class WritePage extends React.Component {
     insertData() {
         const db = this.state.db;
         const data = this.getAllInputs();
+        const date = new Date().toUTCString();
+        var link = document.location.href.split("=");
+        console.log(link)
 
-        update(ref(db, 'users/' + 'AC40BE25-BC94-421A-AB65-76491835F24A'),
+
+        push(ref(db, 'users/' + link[1] + '/notes'),
             {
-                notes: {
-                    UUID: {
-                        // image: data.image,
-                        message: data.message,
-                        sender: data.sender,
-                        // timestamp: data.timestamp
-                    }
-                }
+                image: parseInt(document.querySelector(".selected").id),
+                message: data.message,
+                sender: data.sender,
+                timestamp: date
+
             }).then(() => { alert('데이터 추가 완료') })
             .catch((error) => { alert("데이터 추가 에러" + error) });
     }
 
-    selectData() {
-        const dbref = ref(this.state.db);
-        const UUID = this.getAllInputs().UUID;
-
-        get(child(dbref, 'users/' + UUID)).then((snapshot) => {
-            if (snapshot.exists()) {
-                this.setState({
-                    // image: this.state.image,
-                    message: this.state.message,
-                    sender: this.state.sender,
-                    // timestamp: this.state.timestamp
-                })
-            }
-
-            else {
-                alert("no data found!");
-            }
-        })
-            .catch((error) => { alert("there was an error , details" + error) })
-    }
-
 }
-const Contanier = styledComponents.div`
-	background-color : #f6a58d;
-`;
-
-const Text = styledComponents.div`
-    font-size:20px;
-	font-weight:600;
-`;
 
 
 
