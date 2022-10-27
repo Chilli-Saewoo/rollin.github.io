bc2576be0f5d2720bdfc1738d3602f776688660c
Nick(Seungyun Kim) <sy5072@naver.com>
2022-10-28 04:13:21 +0900

    [feat] 텍스트 수정 (#12)

--- src/pages/sendPage.js
+++ src/pages/sendPage.js
@@ -18,7 +18,7 @@ const SendPage = () => {
   return (
     <div class="send__container">
       <img class="result__image" src={finishImage}></img>
-      <p class="result__text"><strong>{data}</strong>에게<br/>롤링페이퍼가 전달되었어요</p>
+      <p class="result__text"><strong>{data}</strong> 님에게<br/>롤링페이퍼가 전달되었어요</p>
       <p class="result__description">앱을 설치해서 나만의 롤링페이퍼를 만들어보세요</p>
       <form action="https://testflight.apple.com/join/qtUdHgxR">
         <input class="input__testFilght" type="submit" value="테스트 플라이트 설치하기"/>
